3a5163e14980d73e3eaa0d644bf0550bd0ff3bc8
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-27 22:15:10 +0800

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+!# /usr/bin/env bash
+
+# quicklisp
+curl -O http://beta.quicklisp.org/quicklisp.lisp
+curl -O http://beta.quicklisp.org/quicklisp.lisp.asc
+curl -O beta.quicklisp.org/release-key.txt
+mv release-key.txt quicklisp.release.key.asc
+sudo port -v install gnupg
+gpg --import quicklisp.release.key.asc
+gpg --verify quicklisp.lisp.asc
+sudo port -v install sbcl
